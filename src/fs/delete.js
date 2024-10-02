ea31ccb2bd52397c27dcfdac5ff9dd89d7603f06
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-02 10:42:42 +0200

    feat: implement fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { access, constants, unlink } from 'node:fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const directory = join(__dirname, 'files', 'fileToRemove.txt');
+const errorMessage = 'FS operation failed';
+
 const remove = async () => {
-    // Write your code here 
+    await unlink(directory);
 };
 
-await remove();
+try {
+    await access(directory, constants.R_OK | constants.W_OK);
+    await remove();
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+        throw new Error(errorMessage)
+    }
+  }
