18a853c093e5ccd9a33e5c89be90e28a46eb81b8
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-02 16:52:55 +0200

    feat: implement modules esm

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,22 +1,27 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import a from './files/a.json' with { type: 'json' }; 
+import b from './files/b.json' with { type: 'json' }; 
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = a;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = b;
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
@@ -33,7 +38,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
