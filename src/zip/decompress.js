3dc8856175d58730f70dc7f5ef0c24725cd6502d
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-04 10:41:39 +0200

    feat: implement zip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream} from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const archiveDirectory = join(__dirname, 'files', 'archive.gz');
+const distDirectory = join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    const gunzip = createGunzip();
+    const source = createReadStream(archiveDirectory);
+    const destination = createWriteStream(distDirectory);
+    source.pipe(gunzip).pipe(destination);
 };
 
 await decompress();
