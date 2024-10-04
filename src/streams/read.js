e7e403e60ab89b33a46eaa193b652f83158254c2
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-04 09:53:24 +0200

    feat: implement stream read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileDirectory = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream(fileDirectory);
+
+    stream.pipe(stdout);
 };
 
 await read();
