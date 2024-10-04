1105d1cd37df8d581db9fdd05b19361bfb7571f8
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-04 09:46:56 +0200

    feat: implement hash calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileDirectory = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const stream = createReadStream(fileDirectory);
+
+    const hash = createHash('sha256');
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+        const fileHash = hash.digest('hex');
+        console.log(fileHash);
+    });
 };
 
 await calculateHash();
